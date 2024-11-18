b315e58d954e20122a45330e0d51849315db93ab
Tommaso-Zazzaretti <tommaso.zazzaretti.dev@gmail.com>
2024-11-18 09:22:03 +0100

    Styles Fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,17 @@ const App:React.FC<{}> = () => {
   }, []);
 
   return (
-    <Splitter separatorSizePx={1} flexDirection="row" initialRatio={70} min1="20%" min2="10%">
+    <Splitter separatorSizePx={1} flexDirection="row" initialRatio={70}>
 
       <Scroller onScroll={onScrollEventHandler}>
-        <div id="content" style={{width:'30000px', height: '30000px', backgroundColor:'#EEEEFF',  border:'2px solid blue', padding:'20px'}}>
+        <div id="content" style={{width:'30000px', height: '30000px', backgroundColor:'#E1F2FB',  border:'2px solid blue', padding:'20px'}}>
           Right content
         </div>
       </Scroller>
 
       <Splitter separatorSizePx={1} flexDirection="column" initialRatio={10} min1="20%" min2="10%">
-        <div style={{backgroundColor:'#FFEEFF', width:'100%', height:'100%'}}>Left Top content</div>
-        <div style={{backgroundColor:'#FFEEEE', width:'100%', height:'100%'}}>Left Bottom content</div>
+        <div style={{backgroundColor:'#F1F9F9', width:'100%', height:'100%'}}>Left Top content</div>
+        <div style={{backgroundColor:'#E0E8F9', width:'100%', height:'100%'}}>Left Bottom content</div>
       </Splitter>
     </Splitter>
   )
