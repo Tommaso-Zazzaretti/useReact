1dc3b2860773d527add211f27b87b221c72a9d6c
Tommaso-Zazzaretti <tommaso.zazzaretti.dev@gmail.com>
2025-04-01 09:29:05 +0200

    added modal example

---
+++ src/components/Modal/examples/Modal.example.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Fragment } from "react/jsx-runtime";
+import { Modal } from "../Modal";
+
+const Component:React.FC<{}> = () => {
+
+    const [open,setOpen] = React.useState<boolean>(false);
+    
+    return (
+    <Fragment>
+
+        <button onClick={()=>setOpen(true)}></button>
+
+        <Modal open={open} msec={300} back={'rgba(0, 0, 0, 0.3)'} onClose={(reason)=>{setOpen(false);}}>
+            <h2>Modale</h2>
+            <p>Questo è un esempio di modale con trap focus.</p>
+            <input type="text" placeholder="Campo di input" />
+            <button onClick={()=>{setOpen(false);}}>Chiudi</button> 
+        </Modal>
+
+    </Fragment>
+)}
+      
